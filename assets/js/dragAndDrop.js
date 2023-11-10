4379bbed99b214df23f5cdeff4466dc623b773e8
tdcoy510 <tdcoy510@gmail.com>
2023-11-10 17:52:21 -0500

    cleaned up

--- assets/js/dragAndDrop.js
+++
@@ -1,8 +0,0 @@
-const draggables = document.querySelectorAll(".draggable");
-const containers = document.querySelectorAll(".page-box");
-
-draggables.forEach((draggable) => {
-  draggable.addEventListener("dragstart", () => {
-    console.log("drag start");
-  });
-});
